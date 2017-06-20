fbfcd78db2765e4ea20ee710d72cd04ed1f2bf0c
BedirT <mtapkan@na.edu>
2017-06-20 08:54:06 -0500

    Day1 ex added

---
+++ Day1/src/Day1.java
@@ -0,0 +1,19 @@
+public class Day1 {
+
+	// Learning how to use printing on console.
+	
+	public static void main(String[] args) {
+		
+		// Begin with a print w/o new line (print - println)
+		System.out.print("Hello World!");
+		
+		/*
+		 *  This is a comment
+		 */
+		
+		System.out.print(" We have a winner! "); // Space in quot displayed
+		System.out.println(); // New line - ln
+		System.out.printf("%.2f", 3.14159265); // Print formatting
+	}
+
+}
---
+++ Day1/src/Ex1.java
@@ -0,0 +1,17 @@
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("+");
+		System.out.println("++");
+		System.out.println("+++");
+		System.out.println("++++");
+		System.out.println("+++++");
+		System.out.println("++++++");
+		System.out.println("+++++++");
+		System.out.println("++++++++");
+
+	}
+
+}
